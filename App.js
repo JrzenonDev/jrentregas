d0bb92a28e18c2a0f2179110d4baacfb22042616
JrzenonDev <jrobertoonb@gmail.com>
2021-06-04 23:33:19 -0300

    chore: remoção de códigos didaticos/teste para iniciação do projeto

--- App.js
+++ App.js
@@ -2,31 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { Text, View, Button, Alert } from 'react-native';
 import {css} from './assets/css/Css';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 import Page from './views/Page';
 
 export default function App() {
 
-  const [product, setProduct] = useState('carro');
-  const [quantity, setQuantity] = useState(0);
-
-  // controla efeitos colateriais, corre a func quando muda a quantity
-  useEffect(() => {
-    Alert.alert('Um novo produto foi adicionado');
-  }, [quantity]);
-
-  const props = {
-    empresa: 'JR Dev',
-    name: 'José Roberto',
-    produto: product,
-    quantidade: quantity
-  };
+  const Stack = createStackNavigator();
 
   return (
-    <View style={css.container}>
-      <Text>{product}</Text>
-      <Page {...props} />
-      <Button title='Adicionar produtos' onPress={() => setQuantity(quantity + 1)} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
