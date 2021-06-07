2350ffa27f8b087f00604c7c10d80f5e7ffb2c7d
JrzenonDev <jrobertoonb@gmail.com>
2021-06-06 23:58:11 -0300

    chore: criando modelagem de dados, relacionamentos, migrations, usando has many belongs to

--- Controller.js
+++ Controller.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// rotas
+app.get('/', (req, res) => {
+  res.send("Servidor node rodando");
+});
+
+
 let port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
   console.log('Servidor rodando');
